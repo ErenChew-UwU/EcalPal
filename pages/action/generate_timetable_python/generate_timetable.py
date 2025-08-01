b77d5cf4bc72a142794154692d53404c2804f212
Eren Chew <erenchew05@gmail.com>
2025-08-01 23:56:43 +0800

    Fixed Bug: result is not the best

--- pages/action/generate_timetable_python/generate_timetable.py
+++ pages/action/generate_timetable_python/generate_timetable.py
@@ -6,6 +6,7 @@ from crossover import crossover
 from population import create_population
 from dbconnect import get_connection
 from get_data import calculate_pair_count
+import copy
 
 
 
@@ -14,6 +15,8 @@ def run_ga(batch_ids, conn, generations=MAX_GENERATIONS):
     progress = 0
     best_fitness = 0
     no_improve_count = 0
+    best_individual = None
+    PAIR_COUNT = calculate_pair_count(conn)
     for gen in range(generations):
         # 计算适应度
         population = sorted(population, key=lambda chrom: -calculate_fitness(chrom, conn))
@@ -29,17 +32,18 @@ def run_ga(batch_ids, conn, generations=MAX_GENERATIONS):
             new_population.extend([c1, c2])
 
         population = new_population[:POPULATION_SIZE]
-        current_best = max([calculate_fitness(ind, conn) for ind in population])
 
-        # 如果当前更好，就重置无提升计数器
+        # 更新最优个体
+        current_best = calculate_fitness(population[0], conn)
         if current_best > best_fitness:
             best_fitness = current_best
+            best_individual = copy.deepcopy(population[0])
             no_improve_count = 0
         else:
             no_improve_count += 1
 
-        target = calculate_pair_count(conn) * TARGET_FITNESS_BASE
-
+        # 目标适应度
+        target = PAIR_COUNT * TARGET_FITNESS_BASE
         if best_fitness >= target:
             if __name__ == "__main__":
                 print(f"Early stop at generation {gen + 1}, fitness: {best_fitness} | {target}")
@@ -56,7 +60,7 @@ def run_ga(batch_ids, conn, generations=MAX_GENERATIONS):
                 progresing = int((progress / generations) * 100)
                 print(f"Progress: {progresing}% | Generation {gen + 1}: Best fitness = {best_fitness}")
 
-    return population[0]  # 返回适应度最高个体
+    return best_individual  # 返回适应度最高个体
 
 
 if __name__ == "__main__":
@@ -64,3 +68,5 @@ if __name__ == "__main__":
     batchs = {"B0001","B0002"}
     result = run_ga(batchs, conn)
     print(result)
+    fitness_result = calculate_fitness(result, conn)
+    print(fitness_result)
